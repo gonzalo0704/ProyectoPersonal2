8617da2a372ed9d8f0a6a95bd4e4fd6105c7aae7
gonzalo0704 <32782274+gonzalo0704@users.noreply.github.com>
2017-12-03 19:18:57 -0300

    final
    1

---
+++ scripts/traductor.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+			$("#abstract").click(function(){
+			$("#abstract").css("display","none");
+			$("#abstracti").css("display","block");
+			
+				
+			});
+			$("#abstracti").click(function(){
+			$("#abstracti").css("display","none");
+			$("#abstract").css("display","block");
+			
+				
+			});
+		
+		
+	
+		
+});
+var abstracteningles ="My name is Gonzalo Figueroa, I was born on April 7, 1996 in Uruguay. I currently live in Montevideo. I'm a graduate bachelor of Technology from the Escuela Superior de Informática(ESI),with experience in team work and creation of web pages.I define myself as a conscious person who works hard and pays attention to detail, being flexible and quick to learn new skills. I want to continue developing my personal skills and my ability to communicate in the English language."
+var abstractenespa="Mi nombre es Gonzalo Figueroa, nací el 7 de abril de 1996 en Uruguay. Actualmente vivo en Montevideo. Soy bachiller Tecnológico egresado de la Escuela Superior de Informática, con experiencia en trabajos en equipo y creación de páginas web.Me defino como una persona consciente que trabaja duro y presta atención a los detalles, siendo flexible y rápido para aprender nuevas habilidades. Deseo seguir desarrollando mis competencias personales y mi capacidad de comunicación en el idioma Inglés."
